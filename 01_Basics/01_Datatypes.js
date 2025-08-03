301501a8c464a768e85cfa9886b9291e1c1cc70b
Mohammad Tariq <ariqxd01@gmail.com>
2025-08-03 08:23:47 +0000

    datatype

--- 01_Basics/01_Datatypes.js
+++ 01_Basics/01_Datatypes.js
@@ -1,1 +1,25 @@
 "use strict"; // using this it will treat all js code as newer version
+
+// alert("tariq")
+console.log(3+2)
+console.log("Tariq")
+
+let name = "Tariq"
+let age = 19
+let isLoggin = true
+let temperature = null
+
+// number
+// bigInt
+// String = ""
+// boolean = true / false
+// null = standalone value
+// undefined
+// symbols => uniqness
+
+console.log(typeof "Tariq")
+console.log(typeof 234)
+console.log(typeof True)
+
+console.log(typeof null) // null is of type Object 
+console.log(typeof undefined) // un defined type is undefined

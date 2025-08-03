1cf3c53a29fbb71bd5e3151d345144eababd642e
Mohammad Tariq <ariqxd01@gmail.com>
2025-08-03 14:41:14 +0000

    Conversion Operations

---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,31 @@
+let num = "12"
+
+// console.log(typeof num)
+// console.log(typeof(num))
+
+let newNum = Number(num)
+// console.log(newNum);
+// console.log(typeof newNum);
+
+let someNumber = "123abc"
+let otherNumber = Number(someNumber)
+
+// console.log(someNumber)
+
+// console.log(otherNumber)
+// console.log(typeof otherNumber);
+
+let isloggin = true
+
+let loggin = String(isloggin)
+
+// console.log(loggin)
+// console.log(typeof loggin)
+
+let name = 10
+let number = Boolean(name)
+
+console.log(number);
+console.log(typeof number)
+
+

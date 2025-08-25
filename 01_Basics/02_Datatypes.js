a73a40a2eb97bf255937dea8c7a6853b18ae0560
Mohammad Tariq <ariqxd01@gmail.com>
2025-08-25 17:05:28 +0000

    revise

--- 01_Basics/02_Datatypes.js
+++ 01_Basics/02_Datatypes.js
@@ -1,13 +1,15 @@
 "use strict"; // using this it will treat all js code as newer version
 
-// alert("tariq")
+// alert("tariq") // will not work coz it's not in browser
+
 console.log(3+2)
 console.log("Tariq")
 
 let name = "Tariq"
 let age = 19
 let isLoggin = true
-let temperature = null
+let temperature = null // yup null is also a data type in js 
+// for eg. if there is an error then it will senf 0 but it's a value so we will use null
 
 // number
 // bigInt
@@ -22,4 +24,7 @@ console.log(typeof 234)
 console.log(typeof True)
 
 console.log(typeof null) // null is of type Object 
-console.log(typeof undefined) // un defined type is undefined
+console.log(typeof undefined) // undefined type is undefined
+
+console.log(typeof(null));
+
--- 01_Basics/03_ConversionOperations.js
+++ 01_Basics/03_ConversionOperations.js
@@ -1,29 +1,29 @@
 let num = "12"
 
-// console.log(typeof num)
-// console.log(typeof(num))
+console.log(typeof num)
+console.log(typeof(num))
 
 let newNum = Number(num)
-// console.log(newNum);
-// console.log(typeof newNum);
+console.log(newNum);
+console.log(typeof newNum);
 
 let someNumber = "123abc"
 let otherNumber = Number(someNumber)
 
-// console.log(someNumber)
+console.log(someNumber)
 
-// console.log(otherNumber)
-// console.log(typeof otherNumber);
+console.log(otherNumber)
+console.log(typeof otherNumber);
 
 let isloggin = true
 
 let loggin = String(isloggin)
 
-// console.log(loggin)
-// console.log(typeof loggin)
+console.log(loggin)
+console.log(typeof loggin)
 
-let name = 10
-let number = Boolean(name)
+let hi = 10
+let number = Boolean(hi)
 
 console.log(number);
 console.log(typeof number)

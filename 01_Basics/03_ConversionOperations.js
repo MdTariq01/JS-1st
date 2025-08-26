bf4fd0f8ccf7950e7f76d60cbae60498788e1a21
Mohammad Tariq <ariqxd01@gmail.com>
2025-08-26 06:43:24 +0000

    operations

--- 01_Basics/03_ConversionOperations.js
+++ 01_Basics/03_ConversionOperations.js
@@ -1,22 +1,54 @@
 let num = "33"
 let otherNum = Number(num)
 
-console.log(otherNum);
-console.log(typeof otherNum);
+// console.log(otherNum);
+// console.log(typeof otherNum);
 
 let password = "124abc"
 
 let newPassword = Number(password)
 
-console.log(newPassword)
-console.log(typeof newPassword);
+// console.log(newPassword)
+// console.log(typeof newPassword);
 
 let isset = true
 
 let realset = String(isset)
 
-console.log(realset);
-console.log(typeof realset);
+// console.log(realset);
+// console.log(typeof realset);
+
+// ******************OPERATIONS************************
+
+/* 
+
+console.log(3+2); // addition
+console.log(3-3); // substration
+console.log(3*3); // multiply
+console.log(3**3); // power as 3^3
+console.log(3/3); // divition
+console.log(3%3); // remainder
+
+*/
+
+// always try to write clean code as clean as possible
+
+// console.log(1+ "2");
+// console.log("1" + 2);
+
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+
+console.log(+"a"); // adding + acts as type conversion to Number like [Number()]
+
+// increment operator by mdn 
+
+
+
+
+
 
 
 

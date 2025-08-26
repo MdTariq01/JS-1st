4502399b25a4fa382dfcab8cfedddd3f6f6b926d
Mohammad Tariq <ariqxd01@gmail.com>
2025-08-26 06:05:30 +0000

    recape of so far

---
+++ 01_Basics/recape.js
@@ -0,0 +1,9 @@
+// so far i have learned about 3 topics:- Variables, datatypes and conversion operations
+
+// we use 2 types of variables - let and const (var is also there but it has a scoping problem)
+
+// after that we learned about datatypes- number , bigInt , string , float , boolean , null , speacial character
+
+// null is a special type it is a object a
+
+// type conversion we can convert any datatype by writing it as method and the first letter must be capital

1d5eac7e32965df8f1bd4198dd4e72319026c68a
Mohammad Tariq <ariqxd01@gmail.com>
2025-08-27 17:50:17 +0000

    String

--- 01_Basics/05_String.js
+++ 01_Basics/05_String.js
@@ -5,8 +5,22 @@ console.log(`My name is ${name} and my total repos are ${repocount}`);
 
 const gameName = new String("Tariq")
 
+// console.log(gameName.__promo__);
+
+
 console.log(gameName.charAt(3));
 
 console.log(gameName.slice(0,4));
 
 console.log(gameName.codePointAt(3));
+
+console.log(gameName.substring(0,3));
+
+console.log(gameName.slice(-4,4));
+
+const newName = new String("   Dhoni    ")
+
+console.log(newName.trim());
+
+
+

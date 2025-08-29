7c1259399b8351680b3ab2887e4d50b05ff2a001
Mohammad Tariq <ariqxd01@gmail.com>
2025-08-29 18:29:28 +0000

    array

---
+++ 02_Basics/Arrays.js
@@ -0,0 +1,25 @@
+const myArray = [0 , 1 , 2 , 3 , 4 , true , "tariq"]; // array can have any element
+
+// arrays in java are resizable 
+
+const array = [0 , 1 , 2 , 3 , 4 , 5 , 6]
+
+console.log(array[5]);
+
+const myheroes = new Array("Ironman" , "spiderman")
+
+console.log(myheroes);
+
+// array methods
+
+myheroes.push("Captain america") // .push adds an element in the last place
+
+myheroes.pop(); // this will remove the last element of the array
+
+console.log(myheroes.unshift(""));
+
+
+console.log(myheroes)
+
+
+// pop shife unshift includes indexof .join slice splice 

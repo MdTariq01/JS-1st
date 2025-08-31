c612c4c57516366ad46c98620e21abbf20d8dd2a
Mohammad Tariq <ariqxd01@gmail.com>
2025-08-31 12:28:45 +0000

    function 02

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -33,8 +33,44 @@ function greeting (username) { // you can give the parameter a defalte value lik
     return `${username} just loggedin`
 }
 
-const greet = greeting() // if argument not given then it's undefined
+// const greet = greeting() // if argument not given then it's undefined
 
-console.log(greet);
+// console.log(greet);
 
+
+// +++++++++++++++ shopping cart +++++++++++++++++++++(Number of arguments are not known) 
+
+function cartprice (val1 , val2 , ...num1) { // (...) is rest operator used
+    return num1
+}
+
+// console.log(cartprice(200,500,600,700)); // so the first is val1 , second is val2 and the remaining is in num1 which has rest operator
+
+// ++++++++++ funtion in object ++++++++++++++
+
+const user = {
+    username: "Tariq" ,
+    price: 133
+}
+
+function objectfunction(anyobject) {
+    return `Name is ${anyobject.username} and price is ${anyobject.price}`
+}
+
+// console.log(objectfunction(user));
+
+console.log(objectfunction({
+    username: "MD TARIQ" ,
+    price: 134
+}));
+
+const arr = [ 1 , 2 , 3 , 4 , 5 ]
+
+function myArray(array) {
+    return array[1]
+}
+
+console.log(myArray(arr));
+
+console.log(myArray([3,5,6,6]));
 

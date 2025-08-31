15dc95d4627ff36a073059ba3b2e9660128f2828
Mohammad Tariq <ariqxd01@gmail.com>
2025-08-31 15:07:17 +0000

    scope 02

--- 03_Basics/02_Scopes.js
+++ 03_Basics/02_Scopes.js
@@ -8,10 +8,47 @@ if (true) {
     
 }
 
-console.log("Outer" , a); // value of a is still 200 not 10 that was inside the block
+// console.log("Outer" , a); // value of a is still 200 not 10 that was inside the block
 
 
 // console.log(a) 
 // console.log(b) 
-console.log(c) // var is coming out of the if so it has scoping problem
+// console.log(c) // var is coming out of the if so it has scoping problem
+
+
+function one() {
+    const username = "tariq"
+
+    function two() {
+        const website = "youtube"
+        console.log(username)
+    
+    }
+    // console.log(website);
+
+    two()
+}
+
+one()
+
+if(true){
+    const name = "Tariq"
+    if(name === "Tariq") {
+        console.log(name)
+        const website = "youtube"
+    }
+
+    // console.log(website);
+}
+
+// ++++++++++++++ Interesting +++++++++++++++++++++
+addone(6) // will work
+function addone(num){
+    return num+=1
+}
+
+addtwo(6) // will not work as the function is initialized in the next line 
+const addtwo = function(num) {
+    return num+=2
+}
 

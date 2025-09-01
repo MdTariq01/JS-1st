9a659ee97921012a1d44c139da96fef61003b0a7
Mohammad Tariq <ariqxd01@gmail.com>
2025-09-01 17:21:45 +0000

    iife

---
+++ 03_Basics/04_IIFE.js
@@ -0,0 +1,18 @@
+// IMMEDIATELY INVOKED FUNCTION EXPRESSION
+
+// iife isused becaues it instantly executs and it don't get polluted by global scope
+
+// to write iife just cover funtion with () and after that put another () just next to it
+// eg.
+
+(function chai () {
+    console.log(`db connected`);   
+})(); // to give parameter you can give in last paranthesis
+// it is also called named iife
+
+// no named iife- also called arrow function iife
+( (name) => {
+    console.log(`${name} just connected the db`);
+})("Tariq");
+
+// to run two iife side by side put ; after the first one

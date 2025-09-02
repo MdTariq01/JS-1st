24bf47c4c8dde440472b769a3ed0b689f548fdf2
Mohammad Tariq <ariqxd01@gmail.com>
2025-09-02 16:01:49 +0000

    while and do while

---
+++ 05_Iterations/two.js
@@ -0,0 +1,31 @@
+// while loop syntax
+
+// while (condition) {
+    
+// }
+
+let index = 0
+while (index <= 10) {
+    // console.log(`value is ${index}`)
+    index = index + 1
+}
+
+let myarray = ["luffy" , "zoro" , "sanji"]
+// console.log(`The monster trio is here - `);
+let i = 0;
+while (i < myarray.length) {
+    // console.log(myarray[i]);
+    i++;
+}
+
+// do while loop syntax
+
+// do {
+
+// } while();
+
+let number = 0;
+do {
+    console.log("number is " + number);
+    number++;
+} while (number < 11);

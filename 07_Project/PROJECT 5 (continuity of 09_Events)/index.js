ad40fb2cc5613e5837910686e10afa0d4b5dfa0d
Mohammad Tariq <ariqxd01@gmail.com>
2025-09-09 17:29:27 +0530

    PROJECT 5

---
+++ 07_Project/PROJECT 5 (continuity of 09_Events)/index.js
@@ -0,0 +1,28 @@
+let body = document.querySelector('body');
+
+const randomColor = function () {
+  let hex = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+// console.log(randomColor());
+let intervalId;
+
+document.querySelector('#start').addEventListener('click', function () {
+  if (!intervalId) {
+    intervalId = setInterval(changeBackground, 1000);
+  }
+
+  function changeBackground() {
+    document.body.style.backgroundColor = randomColor();
+  }
+});
+
+document.querySelector('#stop').addEventListener('click', function () {
+  clearInterval(intervalId);
+  intervalId = null;
+});
